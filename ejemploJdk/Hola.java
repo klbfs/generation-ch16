726d2a88260efb1c318d73751004b941ea25bc8a
klbfs <friendphoebe@hotmail.es>
2022-08-16 12:48:38 -0500

    Java en terminal

---
+++ ejemploJdk/Hola.java
@@ -0,0 +1,22 @@
+//Esta es una clase del JDK Java
+public class Hola{
+
+	
+
+	public static void main(String arg []){
+	//esta es la declaración del método main
+
+		String miVariable = "Hola mundo soy";
+		String miNombre = "Karen";
+		System.out.println (miVariable +" "+ miNombre);
+
+	}
+}
+
+
+//Modificadores de acceso:
+//Modificador de acceso por default
+//class Hola{}
+
+//Modificador de acceso publico
+//public class Hola{}
