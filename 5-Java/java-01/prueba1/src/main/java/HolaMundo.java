53db49a00bf3063e3817e57fcce7f2c2308413c1
klbfs <friendphoebe@hotmail.es>
2022-08-16 16:16:10 -0500

    java

---
+++ 5-Java/java-01/prueba1/src/main/java/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        String miNombre = "Karen";
+        String mensaje = "Hola Mundo te saluda ";
+        System.out.println(mensaje + miNombre);
+
+    }
+}
